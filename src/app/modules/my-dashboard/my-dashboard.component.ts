4d0cc406ff830ce89706092a4ae181d1d4ac26d6
ravisah33 <ravilive33@gmail.com>
2021-09-20 14:29:58 +0530

    Update my-dashboard.component.ts

--- src/app/modules/my-dashboard/my-dashboard.component.ts
+++ src/app/modules/my-dashboard/my-dashboard.component.ts
@@ -16,8 +16,7 @@ export class MyDashboardComponent implements OnInit {
   Highcharts: typeof Highcharts = Highcharts;
   chartOptions!: Highcharts.Options;
 
-  constructor(
-    private commonService: CommonService) { }
+  constructor(private commonService: CommonService) {}
 
   ngOnInit() {
     this.getDashboardData();
@@ -26,139 +25,114 @@ export class MyDashboardComponent implements OnInit {
   //launches data
   getDashboardData() {
     //Upcomming
-    this.commonService.getAPI(`launches/upcoming`).subscribe((resp) => {
-      if (resp) {
-        this.upcomingLaunchesList = resp;
+    this.commonService.getAPI(`launches/upcoming`).subscribe(
+      (resp) => {
+        if (resp) {
+          this.upcomingLaunchesList = resp;
+        }
+      },
+      (error) => {
+        console.log(error);
       }
-    }, (error) => {
-      console.log(error);
-    });
+    );
     //Past
-    this.commonService.getAPI(`launches/past`).subscribe((resp) => {
-      if (resp) {
-        this.pastLaunchesList = resp;
+    this.commonService.getAPI(`launches/past`).subscribe(
+      (resp) => {
+        if (resp) {
+          this.pastLaunchesList = resp;
+        }
+      },
+      (error) => {
+        console.log(error);
       }
-    }, (error) => {
-      console.log(error);
-    });
+    );
     //All
-    this.commonService.getAPI(`launches`).subscribe((resp) => {
-      if (resp) {
-        this.allLaunchesList = resp;
-        let yearLaunch: any = {};
-        this.allLaunchesList.forEach((element: any) => {
-          if (yearLaunch.hasOwnProperty(element.launch_year)) {
-            const val = yearLaunch[element.launch_year];
-            yearLaunch[element.launch_year] = val + 1;
-          } else {
-            yearLaunch[element.launch_year] = 0;
-          }
-        });
-        console.log('yearLaunch', yearLaunch);
-        this.chartOptions = {
-          series: [
-            {
-              type: "line",
-              data: [1,2,3,4,5,6,7]
+    this.commonService.getAPI(`launches`).subscribe(
+      (resp) => {
+        if (resp) {
+          this.allLaunchesList = resp;
+          let yearLaunch: any = {};
+          this.allLaunchesList.forEach((element: any) => {
+            if (yearLaunch.hasOwnProperty(element.launch_year)) {
+              const val = yearLaunch[element.launch_year];
+              yearLaunch[element.launch_year] = val + 1;
+            } else {
+              yearLaunch[element.launch_year] = 0;
             }
-          ]
-        };
+          });
+          console.log('yearLaunch', yearLaunch);
+          this.createLaunchOverTimeChart(yearLaunch);
+        }
+      },
+      (error) => {
+        console.log(error);
       }
-    }, (error) => {
-      console.log(error);
-    });
+    );
     //All launchpads
-    this.commonService.getAPI(`launchpads`).subscribe((resp) => {
-      if (resp) {
-        this.allLaunchpadsList = resp;
+    this.commonService.getAPI(`launchpads`).subscribe(
+      (resp) => {
+        if (resp) {
+          this.allLaunchpadsList = resp;
+        }
+      },
+      (error) => {
+        console.log(error);
       }
-    }, (error) => {
-      console.log(error);
-    });
+    );
   }
 
   formatDate(d: any) {
     return d;
   }
 
-  createLaunchOverTimeChart() {
-    // Highcharts.chart('launchovertime', {
-
-    //   chart: {
-    //     type: 'column'
-    //   },
-
-    //   title: {
-    //     text: 'Highcharts responsive chart'
-    //   },
-
-    //   subtitle: {
-    //     text: 'Resize the frame or click buttons to change appearance'
-    //   },
-
-    //   legend: {
-    //     align: 'right',
-    //     verticalAlign: 'middle',
-    //     layout: 'vertical'
-    //   },
-
-    //   xAxis: {
-    //     categories: ['Apples', 'Oranges', 'Bananas'],
-    //     labels: {
-    //       x: -10
-    //     }
-    //   },
-
-    //   yAxis: {
-    //     allowDecimals: false,
-    //     title: {
-    //       text: 'Amount'
-    //     }
-    //   },
-
-    //   series: [{
-    //     name: 'Christmas Eve',
-    //     data: [1, 4, 3]
-    //   }, {
-    //     name: 'Christmas Day before dinner',
-    //     data: [6, 4, 2]
-    //   }, {
-    //     name: 'Christmas Day after dinner',
-    //     data: [8, 4, 3]
-    //   }],
-
-    //   responsive: {
-    //     rules: [{
-    //       condition: {
-    //         maxWidth: 500
-    //       },
-    //       chartOptions: {
-    //         legend: {
-    //           align: 'center',
-    //           verticalAlign: 'bottom',
-    //           layout: 'horizontal'
-    //         },
-    //         yAxis: {
-    //           labels: {
-    //             align: 'left',
-    //             x: 0,
-    //             y: -5
-    //           },
-    //           title: {
-    //             text: null
-    //           }
-    //         },
-    //         subtitle: {
-    //           text: null
-    //         },
-    //         credits: {
-    //           enabled: false
-    //         }
-    //       }
-    //     }]
-    //   }
-    // });
-
+  createLaunchOverTimeChart(data) {
+    const d1 = [];
+    const dataObj = Object.keys(data);
+    dataObj.map((itm) => {
+      d1.push([itm, data[itm]]);
+      return itm;
+    });
+    this.chartOptions = {
+      title: {
+        text: '',
+      },
+      xAxis: {
+        title: {
+          text: 'Year',
+        },
+        type: 'category',
+        labels: {
+          rotation: -45,
+          style: {
+            fontSize: '10px',
+            fontFamily: 'Verdana, sans-serif',
+          },
+        },
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Count',
+        },
+      },
+      series: [
+        {
+          type: 'column',
+          data: d1,
+          dataLabels: {
+            enabled: true,
+            rotation: -90,
+            color: '#000',
+            align: 'right',
+            format: '{point.y:.1f}', // one decimal
+            y: 10, // 10 pixels down from the top
+            style: {
+              fontSize: '9px',
+              fontFamily: 'Verdana, sans-serif',
+            },
+          },
+        },
+      ],
+    };
   }
 }
-
